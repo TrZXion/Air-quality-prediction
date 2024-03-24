2ff81543c738f584f7b6ad3d51271a7229863da9
TrZXion <hulkofsteel24@gmail.com>
2024-03-25 04:56:29 +0530

    Added Pollutants API

--- pages/Prediction.py
+++ pages/Prediction.py
@@ -1,9 +1,58 @@
+# trunk-ignore-all(isort)
 import streamlit as st
 import pickle
 import sklearn
 import pandas as pd
 import numpy as np
 from PIL import Image
+import streamlit as st
+from streamlit_js_eval import streamlit_js_eval, get_geolocation
+import requests
+
+
+# Function to make API call using latitude and longitude
+def make_api_call(latitude, longitude):
+    # Your API endpoint URL
+    api_url = f"http://api.openweathermap.org/data/2.5/air_pollution?lat={latitude}&lon={longitude}&appid=9284128446974ce6bf1808e0aced0115"
+    response = requests.get(api_url, timeout=5)
+
+    # Check if request was successful
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        st.error("Error fetching data from API")
+
+
+if st.sidebar.checkbox("Check my location"):
+    loc = get_geolocation()
+
+    # Check if loc is None
+    if loc is not None:
+        # st.write(f"Your coordinates are {loc}")
+        latitude = loc.get("coords", {}).get("latitude")
+        longitude = loc.get("coords", {}).get("longitude")
+
+        if latitude is not None and longitude is not None:
+            # Call the API using latitude and longitude
+            api_data = make_api_call(latitude, longitude)
+
+            # Extract pollutant data from API response
+            pollutants = api_data.get("list", [{}])[0].get("components", {})
+
+            # Display pollutant values using st.metric in sidebar
+            st.sidebar.title("Pollutant Data:")
+            st.sidebar.metric(label="CO", value=pollutants.get("co"))
+            st.sidebar.metric(label="NO", value=pollutants.get("no"))
+            st.sidebar.metric(label="NO2", value=pollutants.get("no2"))
+            st.sidebar.metric(label="O3", value=pollutants.get("o3"))
+            st.sidebar.metric(label="SO2", value=pollutants.get("so2"))
+            st.sidebar.metric(label="PM2.5", value=pollutants.get("pm2_5"))
+            st.sidebar.metric(label="PM10", value=pollutants.get("pm10"))
+            st.sidebar.metric(label="NH3", value=pollutants.get("nh3"))
+        else:
+            st.error("Latitude or longitude is missing.")
+
 
 st.title("Air Quality Particulate Matter Prediction")
 
